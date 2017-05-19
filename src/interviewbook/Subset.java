02af2d3b2b61c9f58491242f43f9ce4e03941d69
willwsxu <willwsxu@gmail.com>
2017-05-19 09:03:47 -0500

    subset recurse

--- src/interviewbook/Subset.java
+++ src/interviewbook/Subset.java
@@ -9,20 +9,28 @@ import java.util.Stack;
 public class Subset {
     
     Stack<Integer> s=new Stack<>();
+    long count=0;
     void recurse(int k, int n)
     {
-        if (k>n)
-            out.println(s.toString()+k);
+        out.println("<<<in "+k);
+        if (k>n) {
+            count++;
+            out.println(s.toString()+" count "+count);
+        }
         else {
             recurse(k+1, n);
-            s.push(k);            
+            s.push(k);    
+            out.println("push "+s);
             recurse(k+1, n);
-            s.pop();            
+            out.println("pop "+s);       
+            s.pop();     
         }
+        out.println(">>out "+k);
     }
     
     public static void main(String[] args)
     {
         new Subset().recurse(1, 2);
+        //new Subset().recurse(1, 3);
     }
 }
