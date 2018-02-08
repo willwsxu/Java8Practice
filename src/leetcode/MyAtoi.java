32fd8256c59ad8cfb848af97855542fb1c035ac4
willwsxu <willwsxu@gmail.com>
2018-02-08 12:15:00 -0600

    atoi check overflow cases

--- src/leetcode/MyAtoi.java
+++ src/leetcode/MyAtoi.java
@@ -8,22 +8,18 @@ public class MyAtoi {
         int val=0;
         int i=0;
         int sign=1;
-        boolean signonce=false;
         for (; i<str.length(); i++) {
             if (Character.isWhitespace(str.charAt(i)))
                 continue;
+            // stop at sign char
             if (str.charAt(i)=='+') {
-                if (signonce)
-                    return 0;
-                signonce=true;
-                continue;
+                i++;
+                break;
             }
             if (str.charAt(i)=='-') {
-                if (signonce)
-                    return 0;
-                signonce=true;
+                i++;
                 sign=-1;
-                continue;
+                break;
             }
             break;
         }
@@ -46,19 +42,33 @@ public class MyAtoi {
             val *=10;
             val += str.charAt(i)-'0';
         }
+        if (i<str.length() && Character.isDigit(str.charAt(i))) {
+            return sign>0?Integer.MAX_VALUE:Integer.MIN_VALUE;
+        }
         val *= sign;
         return val;
     }
     public static void main(String[] args)
     {
-        System.out.println(myAtoi("- 123 3x")==-123);
+        System.out.println(myAtoi("  -123 3x")==-123);
+        System.out.println(myAtoi("  +123 3x")==123);
+        System.out.println(myAtoi("  - 123 3x")==0); // digit must follow first sign
+        System.out.println(myAtoi("  + 123 3x")==0);
+        System.out.println(myAtoi("+-2")==0);
         System.out.println(myAtoi(""));
         System.out.println(myAtoi("    "));
-        System.out.println(myAtoi("+-2")==0);
-        System.out.println(myAtoi(" 2147483648")==Integer.MAX_VALUE);
-        System.out.println(myAtoi("-2147483648")==Integer.MIN_VALUE);
+        System.out.println(myAtoi(" 2147483648")==Integer.MAX_VALUE);  // overflow check for positive
+        System.out.println(myAtoi("-2147483649")==Integer.MIN_VALUE);  // overflow check for negative
+        System.out.println(myAtoi("-2147483648")==Integer.MIN_VALUE);  // negative boundary test
         System.out.println(myAtoi("-2147483647"));
-        System.out.println(myAtoi("-2147483657")==Integer.MIN_VALUE);
+        System.out.println(myAtoi("-2147483657")==Integer.MIN_VALUE);  // more overflow tests
         System.out.println(myAtoi(" 2147483657")==Integer.MAX_VALUE);
+        System.out.println(myAtoi("-21474836471")==Integer.MIN_VALUE);
+        System.out.println(myAtoi(" 21474836471")==Integer.MAX_VALUE);
+        System.out.println(myAtoi("-2147483640x")==-2147483640);
+        System.out.println(myAtoi(" 2147483640x")==2147483640);
+        
+        System.out.println(myAtoi("-21x47483640")==-21);                // not overflow, stop at none digit
+        System.out.println(myAtoi(" 21x47483640")==21);
     }
 }
