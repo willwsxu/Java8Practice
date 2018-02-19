1629fd66c6385ca093c58606c478a054c334f513
ANDY-PROBOOK\Will <willwsxu@gmail.com>
2018-02-19 17:20:37 -0600

    3sum closest

--- src/leetcode/ThreeSum.java
+++ src/leetcode/ThreeSum.java
@@ -47,9 +47,48 @@ public class ThreeSum {
         }
         return ans;
     }
+    static public int threeSumClosest(int[] nums, int target) 
+    {
+        Arrays.sort(nums);
+        int ans=nums[0]+nums[1]+nums[2];
+        int delta=Math.abs(ans-target);
+        for (int i=0; i<nums.length-2; i++) { // try each as first number of 3 sum
+            int sum3=nums[i]+nums[i+1]+nums[i+2];
+            if (sum3==target) 
+                return sum3;
+            else if (sum3>target){
+                if (sum3-target<delta)
+                    return sum3; 
+                return ans;    
+            }
+            if (i>0 && nums[i]==nums[i-1])  // same number, no need to try again
+                continue;
+            int second=i+1;  // pick second and third numbers from both ends
+            int third=nums.length-1;
+            while (second<third) {
+                sum3 = nums[i]+nums[second]+nums[third];
+                int diff=sum3-target;
+                if (diff>delta)
+                    third--;
+                else if (diff<-delta) {
+                    second++;
+                }
+                else {
+                    ans=sum3;
+                    delta=Math.abs(diff);
+                    second++;
+                    third--;
+                }
+            }
+        }
+        return ans;
+    }
     public static void main(String[] args)
     {
         List<List<Integer>> ans=threeSum(new int[]{-10,-10,-10,0,0,20,20});
         System.out.println(ans);
+                
+        System.out.println(threeSumClosest(new int[]{-10,-10,-10,0,0,20,20}, 11));
+        System.out.println(threeSumClosest(new int[]{1,1,-1,2}, 2));
     }
 }
