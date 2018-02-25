2376f208ac14c24740facac2904e98e91f9ed854
ANDY-PROBOOK\Will <willwsxu@gmail.com>
2018-02-25 16:31:50 -0600

    power set, remove dup

--- src/interviewbook/Subset.java
+++ src/interviewbook/Subset.java
@@ -4,6 +4,9 @@
 package interviewbook;
 
 import static java.lang.System.out;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Stack;
 
 public class Subset {
@@ -27,10 +30,49 @@ public class Subset {
         }
         //out.println(">>out "+k);
     }
+    public List<List<Integer>> subsets(int[] nums) {
+        
+        List<Integer> combo=new ArrayList<>();
+        List<List<Integer>> ans=new ArrayList<>();
+        subsets(nums, 0, ans, combo);
+        return ans;        
+    }
+    private void subsets(int[] nums, int k, List<List<Integer>> ans, List<Integer> set) {
+        if (k==nums.length) {
+            ans.add(new ArrayList<>(set));
+            return;
+        }
+        set.add(nums[k]);
+        subsets(nums, k+1, ans, set);
+        set.remove(set.size()-1);
+        subsets(nums, k+1, ans, set);
+    }
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        Arrays.sort(nums);
+        List<Integer> combo=new ArrayList<>();
+        List<List<Integer>> ans=new ArrayList<>();
+        subsetsWithDup(nums, 0, ans, combo);
+        return ans;        
+    }
     
+    private void subsetsWithDup(int[] nums, int k, List<List<Integer>> ans, List<Integer> set) {
+        ans.add(new ArrayList<>(set));
+        for (int i=k; i<nums.length; i++) {
+            if (i>k && nums[i]==nums[i-1])  // skip duplicate
+                continue;
+            set.add(nums[i]);
+            subsetsWithDup(nums, i+1, ans, set);
+            set.remove(set.size()-1);
+        }
+    }
     public static void main(String[] args)
     {
         new Subset().recurse(1, 2);
         new Subset().recurse(1, 3);
+        
+        //List<List<Integer>> ans=new Subset().subsets(new int[]{1,2,3});
+        List<List<Integer>> ans=new Subset().subsetsWithDup(new int[]{1,2,2});
+        for (List<Integer> li: ans)
+            System.out.println(li);
     }
 }
