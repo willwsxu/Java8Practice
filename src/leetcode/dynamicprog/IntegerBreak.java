615cb736210d07f9614c82a8a49d6f661947ecce
Will XU <willwsxu@gmail.com>
2018-03-03 12:06:48 -0600

    integer break dp solution

---
+++ src/leetcode/dynamicprog/IntegerBreak.java
@@ -0,0 +1,32 @@
+// Given a positive integer n, break it into the sum of at least two positive integers and maximize the product of those integers. Return the maximum product you can get. 
+// For example, given n = 2, return 1 (2 = 1 + 1); given n = 10, return 36 (10 = 3 + 3 + 4). 
+// Note: You may assume that n is not less than 2 and not larger than 58. 
+package leetcode.dynamicprog;
+
+import java.util.Arrays;
+
+public class IntegerBreak {
+	int prod[];
+    public int integerBreak(int n) {
+        prod = new int[n+1];
+        prod[0]=0; prod[1]=1;
+        for (int i=2; i<=n; i++) {
+        	int maxp=0;
+        	for (int p=1; p<i; p++) {
+        		int v=p*Integer.max(i-p, prod[i-p] );  // split into 2 numbers, take advantage of previous calculation
+        		if (v>maxp)
+        			maxp=v;
+        	}
+        	prod[i]=maxp;
+        }
+        //System.out.println(Arrays.toString(prod));
+        return prod[n];
+    }
+
+    public static void main(String[] args)
+    {
+    	System.out.println(new IntegerBreak().integerBreak(2)==1);
+    	System.out.println(new IntegerBreak().integerBreak(10)==36);
+    	System.out.println(new IntegerBreak().integerBreak(58)==1549681956);
+    }
+}
