925faad13025ca17b3d763b3e7584ea93ae4d7c2
willwsxu <willwsxu@gmail.com>
2018-03-05 09:53:11 -0600

    count palindrome substring, still slow

--- src/interviewbook/LongestPalinSubStr.java
+++ src/interviewbook/LongestPalinSubStr.java
@@ -46,28 +46,36 @@ public class LongestPalinSubStr {
 
     public int countSubstrings(String s) {
         int n=s.length();
-        dp=new int[n][n];  // len, start index 0
+        dp=new int[2][n];  // only need to keep previous 2 len
         int count=s.length();
-        Arrays.fill(dp[0], 1);
-        for (int len=2; len<=n; len++) {
+        Arrays.fill(dp[0], 1); // len 1
+        
+        for (int j=0; j<n-1; j++) { // len=2
+            if (s.charAt(j+1)!=s.charAt(j)) {
+                dp[1][j]=0;
+            }
+            else {
+                dp[1][j]=2;
+                count++;
+            }
+        }
+        int prevprev=0; // prev=1-prevprev
+        for (int len=3; len<=n; len++) {
             for (int j=0; j<n-len+1; j++) {
                 int end = j+len-1;
                 if (s.charAt(end)!=s.charAt(j)) {
-                    dp[len-1][j]=0;
+                    dp[prevprev][j]=0;  // reuse prevprev for current
                     continue;
                 }
-                if (len==2)
-                    dp[len-1][j]=2;
-                else {
-                    if (dp[len-3][j+1]==0) {
-                        dp[len-1][j]=0;
-                        continue;
-                    }
-                    else
-                        dp[len-1][j] =2+dp[len-3][j+1];
+                if (dp[prevprev][j+1]==0) {  // check value at len-2
+                    dp[prevprev][j]=0;
+                    continue;
                 }
+                else
+                    dp[prevprev][j] =2+dp[prevprev][j+1];
                 count++;
             }
+            prevprev = 1-prevprev; // prev is now preprev
         }
         return count;
     }
@@ -77,5 +85,7 @@ public class LongestPalinSubStr {
 
         out.println(new LongestPalinSubStr().countSubstrings("aaa")==6);
         out.println(new LongestPalinSubStr().countSubstrings("abc")==3);
+        out.println(new LongestPalinSubStr().countSubstrings("a")==1);
+        out.println(new LongestPalinSubStr().countSubstrings("aa")==3);
     }
 }
