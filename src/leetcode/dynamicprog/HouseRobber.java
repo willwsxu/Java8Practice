0fbb5749427becbba47b7cc3338a368a77e56c97
ANDY-PROBOOK\Will <willwsxu@gmail.com>
2018-03-06 18:09:39 -0600

    houes rubber 2

--- src/leetcode/dynamicprog/HouseRobber.java
+++ src/leetcode/dynamicprog/HouseRobber.java
@@ -25,11 +25,37 @@ public class HouseRobber {
         dp[at] = Integer.max(a1,a2);
         return dp[at];
     }
+    
+    // nums in a circle
+    public int rob2(int[] nums) {
+        if (nums.length==0)
+            return 0;
+        if (nums.length==1)
+            return nums[0];
+        dp=new int[nums.length];
+        Arrays.fill(dp,-1);
+        
+        int last=nums[nums.length-1];
+        nums[nums.length-1]=0;
+        int a1=nums[0]+rob(nums, 2);
+        nums[nums.length-1]=last;
+        Arrays.fill(dp,-1); // reset
+        int a2=rob(nums,1);
+        //System.out.println(a1+" "+a2);
+        //System.out.println(Arrays.toString(nums));
+        return Integer.max(a1, a2);
+    }
     public static void main(String[] args)
     {
         System.out.println(new HouseRobber().rob(new int[]{1,1000,2,3,3000})==4000);
+        System.out.println(new HouseRobber().rob(new int[]{1000,2,3,3000})==4000);
         System.out.println(new HouseRobber().rob(new int[]{1})==1);
         System.out.println(new HouseRobber().rob(new int[]{1,2})==2);
         System.out.println(new HouseRobber().rob(new int[]{1,2,0})==2);
+        
+        System.out.println(new HouseRobber().rob2(new int[]{1,1000,2,3,3000})==4000);
+        System.out.println(new HouseRobber().rob2(new int[]{1000,1,2,3,3000})==3002);
+        System.out.println(new HouseRobber().rob2(new int[]{})==0);
+        System.out.println(new HouseRobber().rob2(new int[]{1})==1);
     }
 }
