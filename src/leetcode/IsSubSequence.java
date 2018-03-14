4afa4ee8c8c8c1da20eb59679acfe2949463e329
willwsxu <willwsxu@gmail.com>
2018-03-14 13:06:31 -0500

    is sub sequence TLE

---
+++ src/leetcode/IsSubSequence.java
@@ -0,0 +1,50 @@
+/*
+ * Given a string s and a string t, check if s is subsequence of t
+ */
+package leetcode;
+
+import java.util.Arrays;
+
+public class IsSubSequence {
+    
+    int dp[][];
+    boolean isSubsequence(String s, String t, int i, int j)
+    {
+        if (s.length()==i)  // find all
+            return true;        
+        if (t.length()==j)  // no more char in t to find
+            return false;
+        if (dp[i][j]>=0)
+            return dp[i][j]>0;
+        for (int k=j; k<t.length(); k++)
+            if (t.charAt(k)==s.charAt(i)) {
+                boolean ans = isSubsequence(s, t, i+1, k+1);  // find a match, chech next char in s
+                if (ans) {
+                    dp[i][j]=1;
+                    return true;
+                }
+            }
+        dp[i][j]=0;
+        return false;
+    }
+    public boolean isSubsequence(String s, String t) {
+        if (s.isEmpty())
+            return true;
+        if (t.isEmpty())
+            return false;
+        dp = new int[s.length()][t.length()];
+        for (int [] r: dp)
+            Arrays.fill(r, -1);
+        for (int j=0; j<t.length(); j++) {
+            if (isSubsequence(s,t,0,j))
+                return true;
+        }
+        return false;
+    }
+    
+    public static void main(String[] args)
+    {
+        System.out.println(new IsSubSequence().isSubsequence("abc", "ahbgdc"));
+        System.out.println(new IsSubSequence().isSubsequence("acb", "ahbgdc")==false);
+    }
+}
