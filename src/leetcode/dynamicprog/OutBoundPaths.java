b23d89a03e8bd6a237a69303c95afbe8e5538d4b
Will XU <willwsxu@gmail.com>
2018-03-22 20:10:41 -0500

    out of bound paths dp, beat 88%

--- src/leetcode/dynamicprog/OutBoundPaths.java
+++ src/leetcode/dynamicprog/OutBoundPaths.java
@@ -3,26 +3,41 @@
 // of paths to move the ball out of grid boundary.The answer may be very large, return it after mod 109 + 7.
 package leetcode.dynamicprog;
 
+import java.util.Arrays;
+
 public class OutBoundPaths {
+	int dp[][][];
+	final int MOD=1000000007;
     public int FindPaths(int m, int n, int N, int i, int j, String dir)
     {
         if (N < 0)
             return 0;
         if (i < 0 || i >= m || j < 0 || j >= n)
         {
-            System.out.println(dir +" N="+N+" i="+i+" j="+j);
+            //System.out.println(dir +" N="+N+" i="+i+" j="+j);
             return 1;
         }
         if (N == 0)
             return 0;
-        int count = FindPaths(m, n, N - 1, i-1, j, "up"); // up
-        count  +=   FindPaths(m, n, N - 1, i+1, j, "down"); // down
-        count  +=   FindPaths(m, n, N - 1, i, j-1, "left"); // left
-        count  +=   FindPaths(m, n, N - 1, i, j+1, "right"); // right
+        if (dp[N][i][j]>=0)
+        	return dp[N][i][j];
+        int count = FindPaths(m, n, N - 1, i-1, j, "up")%MOD; // up
+        count  = (count +  FindPaths(m, n, N - 1, i+1, j, "down"))%MOD; // down
+        count  = (count +  FindPaths(m, n, N - 1, i, j-1, "left"))%MOD; // left
+        count  = (count +  FindPaths(m, n, N - 1, i, j+1, "right"))%MOD; // right
+        dp[N][i][j]=count;
         return count;
+    }
+    public int FindPaths(int m, int n, int N, int i, int j)
+    {
+    	dp=new int[N+1][m][n];
+    	for (int rc[][]: dp)
+    		for (int r[]: rc)
+    			Arrays.fill(r, -1);
+    	return FindPaths(m, n, N, i, j, "");
     }
     public static void main(String[] args)
     {
-        System.out.println(new OutBoundPaths().FindPaths(1, 3, 3, 0, 1,"start") );
+        System.out.println(new OutBoundPaths().FindPaths(1, 3, 3, 0, 1) );
     }
 }
