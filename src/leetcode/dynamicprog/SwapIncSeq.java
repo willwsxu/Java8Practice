26b81f1dd56962ef07dfdb1a8285b9f9526a60bf
Will XU <willwsxu@gmail.com>
2018-03-29 23:07:23 -0500

    swap inc seq, pass without dp

--- src/leetcode/dynamicprog/SwapIncSeq.java
+++ src/leetcode/dynamicprog/SwapIncSeq.java
@@ -16,25 +16,41 @@ public class SwapIncSeq {
 		A[p] ^= B[p];
 	}
 	int dp[];
-    public int minSwap(int[] A, int[] B, int pos) {
+    public int minSwap(int[] A, int[] B, int pos, boolean swapprev) {
         if (pos==A.length)
         	return 0;
-        if (dp[pos]>=0)
+        //if (dp[pos]>=0)
+        //	return dp[pos];
+        if (A[pos]==B[pos]) {
+        	dp[pos] = minSwap(A, B, pos+1, false);
         	return dp[pos];
+        }
         int v1=Integer.MAX_VALUE/2;
-        if (pos==0 || A[pos]>A[pos-1]&&B[pos]>B[pos-1])
-        	v1 = minSwap(A, B, pos+1);
-        swap(A, B, pos);
+        int prevA=0;
+        int prevB=0;
+        if (pos>0 && swapprev ) {
+        	prevA=B[pos-1];
+        	prevB=A[pos-1];
+        } else if (pos>0) {
+        	prevA=A[pos-1];
+        	prevB=B[pos-1];
+        }
+        if (pos==0 || A[pos]>prevA&&B[pos]>prevB)
+        	v1 = minSwap(A, B, pos+1, false);
+        //swap(A, B, pos);
         int v2=Integer.MAX_VALUE/2;
-        if (pos==0 || A[pos]>A[pos-1]&&B[pos]>B[pos-1])
-        	v2=1+minSwap(A, B, pos+1);
+        if (pos==0 || A[pos]>prevB&&B[pos]>prevA)
+        	v2=1+minSwap(A, B, pos+1, true);
+        //swap(A, B, pos);
         dp[pos] = Integer.min(v1, v2);
         return dp[pos];
     }
     public int minSwap(int[] A, int[] B) {
     	dp=new int[A.length];
     	Arrays.fill(dp, -1);
-    	return minSwap(A, B, 0);
+    	int ans= minSwap(A, B, 0, false);
+    	System.out.println(Arrays.toString(dp));
+    	return ans;
     }
     public static void main(String[] args)
     {
