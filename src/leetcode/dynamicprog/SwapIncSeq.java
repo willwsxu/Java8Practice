cf5c748187ba3a90afd02a21c871447df86bd250
Will XU <willwsxu@gmail.com>
2018-03-29 23:15:09 -0500

    swap inc seq, fix dp, pass

--- src/leetcode/dynamicprog/SwapIncSeq.java
+++ src/leetcode/dynamicprog/SwapIncSeq.java
@@ -15,20 +15,20 @@ public class SwapIncSeq {
 		B[p] ^= A[p];
 		A[p] ^= B[p];
 	}
-	int dp[];
-    public int minSwap(int[] A, int[] B, int pos, boolean swapprev) {
+	int dp[][];
+    public int minSwap(int[] A, int[] B, int pos, int swapprev) {
         if (pos==A.length)
         	return 0;
-        //if (dp[pos]>=0)
-        //	return dp[pos];
+        if (dp[swapprev][pos]>=0)
+        	return dp[swapprev][pos];
         if (A[pos]==B[pos]) {
-        	dp[pos] = minSwap(A, B, pos+1, false);
-        	return dp[pos];
+        	dp[swapprev][pos] = minSwap(A, B, pos+1, 0);
+        	return dp[swapprev][pos];
         }
         int v1=Integer.MAX_VALUE/2;
         int prevA=0;
         int prevB=0;
-        if (pos>0 && swapprev ) {
+        if (pos>0 && swapprev>0 ) {
         	prevA=B[pos-1];
         	prevB=A[pos-1];
         } else if (pos>0) {
@@ -36,20 +36,21 @@ public class SwapIncSeq {
         	prevB=B[pos-1];
         }
         if (pos==0 || A[pos]>prevA&&B[pos]>prevB)
-        	v1 = minSwap(A, B, pos+1, false);
+        	v1 = minSwap(A, B, pos+1, 0);
         //swap(A, B, pos);
         int v2=Integer.MAX_VALUE/2;
         if (pos==0 || A[pos]>prevB&&B[pos]>prevA)
-        	v2=1+minSwap(A, B, pos+1, true);
+        	v2=1+minSwap(A, B, pos+1, 1);
         //swap(A, B, pos);
-        dp[pos] = Integer.min(v1, v2);
-        return dp[pos];
+        dp[swapprev][pos] = Integer.min(v1, v2);
+        return dp[swapprev][pos];
     }
     public int minSwap(int[] A, int[] B) {
-    	dp=new int[A.length];
-    	Arrays.fill(dp, -1);
-    	int ans= minSwap(A, B, 0, false);
-    	System.out.println(Arrays.toString(dp));
+    	dp=new int[2][A.length];  // 0 no swap, 1 swap
+    	Arrays.fill(dp[0], -1);
+    	Arrays.fill(dp[1], -1);
+    	int ans= minSwap(A, B, 0, 0);
+    	//System.out.println(Arrays.toString(dp));
     	return ans;
     }
     public static void main(String[] args)
