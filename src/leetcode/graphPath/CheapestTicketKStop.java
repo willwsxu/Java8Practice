2b230df40f751700e6670a6595b7e7286da94bcb
willwsxu <willwsxu@gmail.com>
2018-04-05 09:50:45 -0500

    cheapest ticket k stop, bfs, beat 43%

--- src/leetcode/graphPath/CheapestTicketKStop.java
+++ src/leetcode/graphPath/CheapestTicketKStop.java
@@ -9,40 +9,51 @@ import java.util.concurrent.ArrayBlockingQueue;
 
 public class CheapestTicketKStop {
     static public int findCheapestPrice(int n, int[][] flights, int src, int dst, int K) {
-    	Queue<int[]> nodes=new ArrayBlockingQueue<>(n*n);
+    	Queue<Integer> nodes=new ArrayBlockingQueue<>(2*n);
     	List<List<int[]>> adjList=new ArrayList<>();
     	for (int i=0; i<n; i++)
     		adjList.add(new ArrayList<>());
     	for (int[] f: flights) {
     		adjList.get(f[0]).add(f);
     	}
-    	nodes.add(new int[] {src, 0, 0}); // city, stop, price
+    	nodes.add(src); // city
     	int stops=0;
-    	int ans=Integer.MAX_VALUE;
+        int ans[] = new int[n];
+        Arrays.fill(ans, Integer.MAX_VALUE/2);
+        ans[src]=0;
+        boolean added[]=new boolean[n];
     	while (!nodes.isEmpty() && stops<=K) {
-    		int lastSize=nodes.size();
-    		for (int i=0; i<lastSize; i++) {
-    			int[] s=nodes.poll();
-    			for (int [] f : adjList.get(s[0])) {
-    				if (stops==K && f[1]!=dst)
-    					continue;
-    				if (f[1]==dst) {
-    					ans = Integer.min(ans, s[2]+f[2]);
-    					continue;
-    				}
-    				System.out.println("add "+s[0]+": size"+nodes.size()+" stops "+stops);
-    				nodes.add(new int[]{f[1], s[1]+1, s[2]+f[2]});
-    			}
-    		}
-    		stops++;
+            int lastSize=nodes.size();
+            Arrays.fill(added, false);  // don't add same note twice
+            for (int i=0; i<lastSize; i++) {
+                int from=nodes.poll();  // from city
+                for (int [] f : adjList.get(from)) {
+                    if (stops==K && f[1]!=dst)
+                            continue;
+                    ans[f[1]] = Integer.min(ans[f[1]], ans[from]+f[2]);
+                    if (f[1]==dst) {
+                        continue;
+                    }
+                    //System.out.println(head[0]+" add "+f[1]+": size="+nodes.size()+" stops "+stops);
+                    if ( !added[f[1]]) {
+                        nodes.add(f[1]);
+                        added[f[1]]=true;
+                    }
+                }
+            }
+            //System.out.println(Arrays.toString(ans));
+            stops++;
     	}
-    	return ans;
+    	return ans[dst]>=Integer.MAX_VALUE/2?-1:ans[dst];
     }
     public static void main(String[] args)
     {
     	int [][]flights=new int[][] {{3,4,4},{2,5,6},{4,7,10},{9,6,5},{7,4,4},{6,2,10},{6,8,6},{7,9,4},{1,5,4},{1,0,4},{9,7,3},{7,0,5},{6,5,8},{1,7,6},{4,0,9},{5,9,1},{8,7,3},{1,2,6},{4,1,5},{5,2,4},{1,9,1},{7,8,10},{0,4,2},{7,2,8}};
     	int ans=findCheapestPrice(10, flights, 6, 0, 7);
-    	System.out.println(ans);
+    	System.out.println(ans==14);
+        
+        ans=findCheapestPrice(5, new int[][]{{4,1,1},{1,2,3},{0,3,2},{0,4,10},{3,1,1},{1,4,3}}, 2, 1, 1);
+    	System.out.println(ans==-1);
     }
 
 }
