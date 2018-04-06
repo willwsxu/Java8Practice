456f1a5d60345c7c3e77a097b0bcbefd0440c732
willwsxu <willwsxu@gmail.com>
2018-04-06 10:16:36 -0500

    stock buy 2 transaction, using difference array, tle

--- src/leetcode/dpspecial/BuyStock.java
+++ src/leetcode/dpspecial/BuyStock.java
@@ -1,6 +1,9 @@
 package leetcode.dpspecial;
 
 // Give stock price each day, you can buy and sell any time but cannot buy again without sell 
+
+import java.util.Arrays;
+
 // each transaction must pay a fee
 // 0 < prices.length <= 50000, which mean we need to better than O(n^2)
 public class BuyStock {
@@ -33,9 +36,63 @@ public class BuyStock {
     	return Integer.max(dpBuy, dpSell);
     }
     
+    //Design an algorithm to find the maximum profit. You may complete at most two transactions
+    int getMax(int[] diff, int start, int end) {
+        int sum1=0;
+        int max1=0;
+        for (int i=start; i<end; i++) {
+            sum1 +=diff[i];
+            if (sum1<0)
+                sum1=0;  // start over
+            max1=Integer.max(max1, sum1);
+        }
+        return max1;
+        /*int sum2=0;
+        int max2=0;
+        for (int i=end-1; i>=start; i--) {
+            sum2 +=diff[i];
+            if (sum2<0)
+                sum2=0;  // start over
+            max2=Integer.max(max2, sum2);
+        }
+        return Integer.max(max1, max2);*/
+    }
+    public int maxProfit(int[] prices) {
+        int n=prices.length-1;
+        if (n<1)
+            return 0;
+        int diff[]=new int[n];
+        for (int i=0; i<n; i++)
+            diff[i]=prices[i+1]-prices[i];
+        //System.out.println(Arrays.toString(diff));
+        int ans=0;
+        for (int partition=0; partition<n; partition++) {  // partition difference array in 2 parts
+            int sum1=getMax(diff, 0, partition);
+            int sum2=getMax(diff, partition, n);
+            ans=Integer.max(ans, sum1+sum2);
+            //System.out.println("p="+partition+": "+sum1+" "+sum2);
+        }
+        return ans;
+    }
+    
     public static void main(String[] args)
     {
     	int ans=new BuyStock().maxProfit(new int[] {1, 3, 2, 8, 4, 9}, 2);
+    	System.out.println(ans==8);
+        
+        ans=new BuyStock().maxProfit(new int[] {8,6,4,3,3,2,3,5,8,3,8,2,6});
+    	System.out.println(ans==11);
+        
+        ans=new BuyStock().maxProfit(new int[] {2,1,4,5,2,9,7});
+    	System.out.println(ans);
+       
+        ans=new BuyStock().maxProfit(new int[] {1});
+    	System.out.println(ans==0);
+        
+        ans=new BuyStock().maxProfit(new int[] {});
+    	System.out.println(ans==0);
+        
+        ans=new BuyStock().maxProfit(new int[] {1,9});
     	System.out.println(ans==8);
     }
 }
