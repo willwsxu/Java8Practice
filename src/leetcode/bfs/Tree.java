ea11989c824f29b31eec705751180f35a354cfe1
willwsxu <willwsxu@gmail.com>
2018-04-11 11:48:04 -0500

    tree rigth side view, bfs, beat 50%, right traversal beat 90%

--- src/leetcode/bfs/Tree.java
+++ src/leetcode/bfs/Tree.java
@@ -12,6 +12,7 @@ class TreeNode {
     TreeNode(int x) { val = x; }
 }
 public class Tree {
+    // Given a binary tree, find the leftmost value in the last row of the tree
     static public int findBottomLeftValue(TreeNode root) {
         Queue<TreeNode> q=new ArrayDeque<>();
         q.add(root);
@@ -53,6 +54,46 @@ public class Tree {
         }
         return ans;
     }
+    
+    // Given a binary tree, imagine yourself standing on the right side of it, 
+    // return the values of the nodes you can see ordered from top to bottom.
+    static public List<Integer> rightSideView(TreeNode root) {  // similar idea to findBottomLeftValue
+        List<Integer> ans=new ArrayList<>();
+    	if (root==null)
+    		return ans;
+        Queue<TreeNode> q=new ArrayDeque<>();
+        q.add(root);
+        while (!q.isEmpty()) {
+        	int size=q.size();
+                int last=0;
+        	for (int i=0; i<size; i++) {
+                    TreeNode tn=q.poll();
+                    last=tn.val;
+                    if (tn.left!=null)
+                            q.add(tn.left);
+                    if (tn.right!=null)
+                            q.add(tn.right);        		
+        	}
+                ans.add(last);
+        }
+        return ans;        
+    }
+    
+    static void rightSideView_recur(TreeNode node, List<Integer> ans, int d)
+    {
+        if (node==null)
+            return;
+        if (ans.size()==d)  // first node from right at this level
+            ans.add(node.val);
+        rightSideView_recur(node.right, ans, d+1); // right side first
+        rightSideView_recur(node.left, ans, d+1); // right side first
+    }
+    static public List<Integer> rightSideView_recur(TreeNode root) {  // faster 2 ms vs 3ms in rightSideView
+        List<Integer> ans=new ArrayList<>();
+        rightSideView_recur(root, ans, 0);
+        return ans;
+    }
+    
     public static void main(String[] args)
     {
     	TreeNode r=new TreeNode(1);
@@ -70,5 +111,8 @@ public class Tree {
     		System.out.println(li);
     	}
     	ans=levelOrder(null);
+        
+        System.out.println(rightSideView(r));
+        System.out.println(rightSideView_recur(r));
     }
 }
