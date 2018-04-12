cfb5d10f81b8efb1f0d5d38e0d264fd69af87438
Will XU <willwsxu@gmail.com>
2018-04-11 22:12:37 -0500

    islands, bfs, beat 20%

--- src/leetcode/ConnectedComponents.java
+++ src/leetcode/ConnectedComponents.java
@@ -6,35 +6,40 @@ import java.util.Queue;
 public class ConnectedComponents {
 
     static public int numIslands(char[][] grid) {
-    	int id=0;
+    	int count=0;
     	if (grid==null||grid.length==0)
     		return 0;
     	int m=grid.length; 
     	int n=grid[0].length;
     	int[][] dir=new int[][] {{-1,0},{1,0},{0,-1},{0,1}};
-        for (int i=0; i<grid.length; i++) {
-        	for (int j=0; j<grid[0].length; j++) {
+        for (int i=0; i<m; i++) {
+        	for (int j=0; j<n; j++) {
         		if (grid[i][j] !='1')
         			continue;
         		//fill all connected
-        		id++;
+        		count++;
         		Queue<int[]> q=new ArrayDeque<>();
         		q.add(new int[] {i,j});
+    			grid[i][j] += count; // mark it first!!
         		while (!q.isEmpty()) {
         			int[] loc=q.poll();
-        			grid[loc[0]][loc[1]] += id;
+        			//System.out.println(loc[0]+","+loc[1]+":"+grid[loc[0]][loc[1]]);
         			for (int [] d: dir) {
         				int r=loc[0]+d[0];
         				int c=loc[1]+d[1];
         				if (r<0 || c<0 || r>=m || c>=n) 					
         					continue;
-        				if (grid[r][c]=='1')
+        				if (grid[r][c]=='1') {
+        					grid[r][c] += count; // mark it when add to queue, so it won't be added again
         					q.add(new int[] {r,c});
+        				}
         			}
         		}
+                //for (char [] line: grid)
+                //	System.out.println(line);
         	}
         }
-        return id;
+        return count;
     }
 
     public static void main(String[] args)
@@ -47,5 +52,8 @@ public class ConnectedComponents {
     	System.out.println(numIslands(g)==1);
     	g=new char[][] {{'0'}};
     	System.out.println(numIslands(g)==0);
+    	
+    	g=new char[][] {{'1'},{'0'},{'1'},{'0'},{'1'},{'1'}};
+    	System.out.println(numIslands(g));
     }
 }
