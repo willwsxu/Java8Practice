2a23dfd4b585368700a06332aa9ff65abf74d933
Will XU <willwsxu@gmail.com>
2018-04-12 21:36:00 -0500

    perm methos 2, tle

--- src/leetcode/backtracking/Permutation.java
+++ src/leetcode/backtracking/Permutation.java
@@ -21,13 +21,32 @@ public class Permutation {
 			perm(n, pos+1, s+i, mask|m, k);
 		}
 	}
+	void perm(String s, String target, int k) {
+		if (s.length()==1) {
+			//System.out.println(target+s);
+			if (++count==k)
+				ans=target+s;
+			return;
+		}
+		for (int i=0; i<s.length(); i++) {
+			perm(s.substring(0,i)+s.substring(i+1,s.length()), target+s.charAt(i), k);
+		}
+	}
     public String getPermutation(int n, int k) {
-        perm(n, 0, "", 0, k);
+    	StringBuilder sb=new StringBuilder();
+    	for (int i=1; i<=n; i++)
+    		sb.append(i);
+        perm(sb.toString(), "", k);
+    	//perm(n, 0,"", 0, k);
         return ans;
     }
     public static void main(String[] args)
     {
-    	System.out.println(new Permutation().getPermutation(3,  5));
-    	System.out.println(new Permutation().getPermutation(1,  1));
+    	System.out.println(new Permutation().getPermutation(3,  5).equals("312"));
+    	System.out.println(new Permutation().getPermutation(1,  1).equals("1"));
+    	//for (int i=0; i<100; i++) {
+    	System.out.println(new Permutation().getPermutation(9,  199269).equals("594738216"));
+    	System.out.println(new Permutation().getPermutation(9,  135401).equals("439157826"));
+    	//}
     }
 }
