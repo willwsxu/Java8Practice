82e5c9b368ec1517c8721fbbdc3620183588dd1b
Will XU <willwsxu@gmail.com>
2018-04-16 16:07:05 -0500

    7ms divde and conquer recursively, beat 68%

--- src/leetcode/backtracking/Parentheses.java
+++ src/leetcode/backtracking/Parentheses.java
@@ -23,63 +23,41 @@ public class Parentheses {
 		return ans;
 	}
     
-	void generator(List<String> all, String input, int idx, int n, int open, int close, boolean canClose, String output) {
-		//System.out.println(idx+" "+open+" "+close +" "+canClose+" "+output);
-		if (n==open && close==n ) // done
-		{
-			all.add(output);
-			return;
-		}
-		if (idx==input.length()) {
-			all.add(output+")");
-			return;
-		}
-		if (open<n) {
-			if (Character.isDigit(input.charAt(idx)))
-				generator(all, input, idx, n, open+1, close, false, output+"(");
-			else
-				generator(all, input, idx+1, n, open+1, close, false, output+input.charAt(idx)+"(");
-		}
-		if (open>close) {
-			if (!Character.isDigit(input.charAt(idx))) 
-				generator(all, input, idx+1, n, open, close, true, output+input.charAt(idx));
-			else if (idx<input.length())  {
-				//System.out.println("before "+output);
-				while (idx<input.length() && Character.isDigit(input.charAt(idx))) {
-					output += input.charAt(idx++);
-				}
-				//System.out.println("after "+output);
-				if (canClose)
-					generator(all, input, idx, n, open, close+1, true, output+")");
-				else
-					generator(all, input, idx, n, open, close, false, output);
-			}
-		}
-	}
 	// Given a string of numbers and operators, return all possible results from computing all the different 
 	// possible ways to group numbers and operators. The valid operators are +, - and *.
-    public List<Integer> diffWaysToCompute(String input) { // incorrect
+    public List<Integer> diffWaysToCompute(String input) {
     	int operators=0;
-        for (int i=0; i<input.length(); i++)
-        	if (!Character.isDigit(input.charAt(i)))
+        List<Integer> ans=new ArrayList<>();        
+        for (int i=0; i<input.length(); i++) {
+        	if (!Character.isDigit(input.charAt(i))) {  // find an operator
         		operators++;
-        List<Integer> ans=new ArrayList<>();
-        if (operators==0) {
-        	ans.add(Integer.parseInt(input));
-        	return ans;
+            	String left=input.substring(0, i);
+            	String right=input.substring(i+1, input.length());
+        		List<Integer> ansL=diffWaysToCompute(left); // find answer left side
+        		List<Integer> ansR=diffWaysToCompute(right);// find answer right side
+        		for (int L: ansL) {		// combine answers from each side, applying the operator
+        			for (int R: ansR) {
+        				switch (input.charAt(i)) {
+        				case '+':	ans.add(L+R);	break;
+        				case '-':	ans.add(L-R);	break;
+        				case '*':	ans.add(L*R);	break;
+        				}
+        			}
+        		}
+        	}
         }
-
-        List<String> formula=new ArrayList<>();
-        generator(formula, input,0,operators, 0,0,false, new String());
-        System.out.println(formula);
+        if (operators==0) {  // terminate case, operand only
+        	ans.add(Integer.parseInt(input));
+        }
         return ans;
     }
     public static void main(String[] args)
     {
     	System.out.println(new Parentheses().generateParenthesis(3));    	
-
+    	
     	System.out.println(new Parentheses().diffWaysToCompute("3"));
     	System.out.println(new Parentheses().diffWaysToCompute("3+4"));
-    	System.out.println(new Parentheses().diffWaysToCompute("2-1-1"));
+    	System.out.println(new Parentheses().diffWaysToCompute("2-1-1")); //2,0
+    	System.out.println(new Parentheses().diffWaysToCompute("2*3-4*5"));//-34, -10, -14, -10, 10
     }
 }
