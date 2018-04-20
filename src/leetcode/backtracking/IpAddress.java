02c9503b9ce69b2934fe25c80da3b133396b6a31
Will XU <willwsxu@gmail.com>
2018-04-19 21:25:22 -0500

    ip address more pruning, beat 93%

--- src/leetcode/backtracking/IpAddress.java
+++ src/leetcode/backtracking/IpAddress.java
@@ -4,13 +4,25 @@ import java.util.ArrayList;
 import java.util.List;
 
 // Given a string containing only digits, restore it by returning all possible valid IP address combinations
+// add more pruning to improve performance
+//    not too long, not too short
+//    no for loop in 4th part
 public class IpAddress {
 
 	void backtracking(List<String> ans, String ip, String src, int pos, int count)
 	{
-		if (count==4) {
-			if (pos==src.length())  // all digits used
-				ans.add(ip);
+		int remaining=src.length()-pos;
+		if (remaining>3*(4-count))  // validate length, optimization gain 1 ms (from 5 to 4)
+			return;
+		if (remaining<4-count) // not enough
+			return;
+		if (count==3) { // use all digits
+			if (src.charAt(pos)=='0' && remaining>1) // don't allow 0##
+				return;
+			String p=src.substring(pos, src.length());
+			if (Integer.parseInt(p)>255)  // range between 0 to 255
+				return;
+			ans.add(ip+p);
 			return;
 		}
 		for (int i=1; i<=3 && pos+i<=src.length(); i++) { // at most 3 digits is used in IP
@@ -19,9 +31,7 @@ public class IpAddress {
 			String p=src.substring(pos, pos+i);
 			if (Integer.parseInt(p)>255)  // range between 0 to 255
 				break;
-			if (!ip.isEmpty()) 
-				p='.'+p;
-			backtracking(ans, ip+p, src, pos+i, count+1);	
+			backtracking(ans, ip+p+".", src, pos+i, count+1);	
 		}
 	}
     public List<String> restoreIpAddresses(String s) {
